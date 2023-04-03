3c3e2745fe68a78f5622fbe62b634db4e0a7b578
amir <amiroxx7@gmail.com>
2023-04-03 18:04:28 +0200

    switch

---
+++ ratioed/src/PageProfil.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function PageProfil (props) {
+
+    return (
+        <div>
+
+            <h1>Page Profil</h1>
+        </div>
+    );
+}
+
+export default PageProfil;
