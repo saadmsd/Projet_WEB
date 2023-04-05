10bef6a22214ac344f90794b1a1998b8e5424d8d
amir <amiroxx7@gmail.com>
2023-04-05 23:50:25 +0200

    a

--- ratioed/src/Signup.js
+++ ratioed/src/Signup.js
@@ -1,4 +1,6 @@
 import { useState } from 'react';
+import axios from 'axios';
+import { useHistory } from 'react-router-dom';
 
 function Signin (props) {
 
@@ -19,6 +21,18 @@ function Signin (props) {
     const submissionHandler = (evt) => {
         if (pass1 === pass2) setPassOK(true);
         else setPassOK(false);
+        if (passOK) {
+            axios.post("http://localhost:3001/api/user", {
+                login: login,
+                password: password,
+                firstname: firstName,
+                lastname: lastName
+            }).then((res) => {
+                console.log(res);
+            }).catch((err) => {
+                console.log(err);
+            })
+        }
     }
 
     const resetHandler = (evt) => {
@@ -35,13 +49,13 @@ function Signin (props) {
     return (
         <div>
             <label htmlFor="firstname">First name</label>
-            <label htmlFor="lastname">Last namezzzzz</label>
-            <input id="firstname" value={firstName} onChange={getFirstName}/>
-            <input id="lastname" value={lastName}onChange={getLastName}/>
+            <label htmlFor="lastname">Last name</label>
+            <input id="firstname" value={firstName} onChange={getFirstName} name='firstname' />
+            <input id="lastname" value={lastName}onChange={getLastName} name='lastname' />
             <label htmlFor="signin_login">Login</label>
-            <input id="signin_login" value={login} onChange={getLogin}/>
+            <input id="signin_login" value={login} onChange={getLogin} name='login' />
             <label htmlFor="signin_mdp1">Password</label>
-            <input type="password" value={pass1} id="signin_mdp1" onChange={getPass1}/>
+            <input type="password" value={pass1} id="signin_mdp1" onChange={getPass1} name='password' />
             <label htmlFor="signin_mdp2">Password (2)</label>
             <input type="password" value={pass2} id="signin_mdp2" onChange={getPass2}/>
             <button onClick={submissionHandler}>Sign In</button>
--- server/src/index.js
+++ server/src/index.js
@@ -2,4 +2,4 @@ const app = require("./app.js");
 const port = 4000;
 app.default.listen(port, () => {
   console.log(`Serveur actif sur le port ${port}`);
-});
+}); 
