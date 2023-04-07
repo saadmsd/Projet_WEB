0790a23fdf492e1acb5ddd003e08f7e75e7d76b1
saadmsd <saadmousstaid@gmail.com>
2023-04-07 18:44:56 +0200

    axiosss

--- server/app.js
+++ server/app.js
@@ -9,7 +9,13 @@ const basedir = path.normalize(path.dirname(__dirname));
 console.debug(`Base directory: ${basedir}`);
 
 app.use(express.static(path.join(basedir, 'public')));
-
+// Activer les en-têtes CORS
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+  });
 app.use('/api', router);
 
 module.exports = app; // Export the app instance
