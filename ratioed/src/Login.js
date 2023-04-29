531351a00a4a2cd04d2791fc20f6be433bc3d6f8
saadmsd <saadmousstaid@gmail.com>
2023-04-29 02:21:22 +0200

    alerts psg

--- ratioed/src/Login.js
+++ ratioed/src/Login.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import axios from 'axios';
-
+import Swal from 'sweetalert2';
 function Login (props) {
     const {login, currentUser, setCurrentUser} = props;
     const [loggin, setLoggin] = useState("");
@@ -29,17 +29,30 @@ function Login (props) {
             .catch((error) => {
                 console.log(error);
                 setError(error.response.data.message);
+                Swal.fire({
+                    icon: 'error',
+                    title: 'OUPS !',
+                    text: error.response.data.message,
+                    color: 'red',
+                    confirmButtonText: 'OK',
+                    position: 'center',
+                    backdrop: `
+                        rgba(123,0,0,0.4)
+                        url("paredes.gif")
+                        center top
+                        no-repeat
+                    `
+                })
                 }
             );
     };
 
 
     return (
-        <form>
+        <form className='login'>
             <label htmlFor="loggin">Login</label><input id="login" onChange={getLoggin} name = "login" />
             <label htmlFor="mdp">Mot de passe</label><input type="password" id="mdp" onChange={getPassword} name = "password" />
             <button type="submit" onClick={handleSubmit}>Log In</button>
-            <p>{error}</p>
         </form>
     );
 }
