35871d64779db8b40df9eacc1130811a1d00df67
saadmsd <saadmousstaid@gmail.com>
2023-05-02 03:00:24 +0200

    npm install react-fast-marquee --save

--- ratioed/src/NavigationPanel.js
+++ ratioed/src/NavigationPanel.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import Login from './Login';
 import Logout from './Logout';
 import SearchBar from './SearchBar';
-
+import Marquee from "react-fast-marquee";
 import './style/NavigationPanel.css';
 
 function NavigationPanel(props) {
@@ -23,9 +23,16 @@ function NavigationPanel(props) {
         <nav  className={isTop ? "top" : "scrolled"} id="navigation_pan">
           <img src="colo.ico" alt="Logo de Ratioed" />
             {(props.isConnected) ? 
-            <div className='top-right'>
-                <SearchBar id="form" currentUser={currentUser} getProfile={getProfile} handleProfile={handleProfile} selectedUser={selectedUser} setSelectedUser={setSelectedUser}/>
-                <Logout logout={props.logout} currentUser={currentUser} setCurrentUser={setCurrentUser}/>
+            <div className='topC'>
+              <Marquee autoFill="true" speed="100" className="defile" gradientWidth="0" >
+                <p className="defile-text"> Bienvenue sur Ratio'ed {currentUser} ! </p>
+                <img src="barca.gif" alt="Barça" />
+                <img src="psg.gif" alt="PSG" />
+              </Marquee>
+              <div className='top-right'>
+                  <SearchBar id="form" currentUser={currentUser} getProfile={getProfile} handleProfile={handleProfile} selectedUser={selectedUser} setSelectedUser={setSelectedUser}/>
+                  <Logout logout={props.logout} currentUser={currentUser} setCurrentUser={setCurrentUser}/>
+              </div>
             </div>
             :<Login login={props.login} currentUser={currentUser} setCurrentUser={setCurrentUser}/>}
         </nav>
